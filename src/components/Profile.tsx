7defe289777bafa7674a324b44bf6af8b9975b2e
c-rodrigosouza <rodrigo_souzafs@hotmail.com>
2021-03-05 14:23:26 -0300

    Projeto finalizado

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+﻿import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/66509290?s=460&u=b2447516d51d3c9a1297d78f61d91de6239ec499&v=4" alt="Rodrigo Souza" />
+      <div>
+        <strong>Rodrigo Souza</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  )
+}
